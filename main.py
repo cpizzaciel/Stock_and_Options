b4eabe0bd696306b2ae816f6f03aa09269253272
Xiangnan Sun <xiangnan.sun.apply@gmail.com>
2025-10-19 14:07:33 +0100

    add stock return calculation to main.py

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-import utils.stock_price_fetcher as spf
+import sys
 
+import utils.stock_price_fetcher as spf
+import utils.return_calculation as rc
 
 
 if __name__ == "__main__":
     tickers = ['NVDA', 'KO']
     stock_data = spf.fetch_stock_price_history(tickers, '1y', '1d')
-    if stock_data is not None and not stock_data.empty:
-        print("--- Data in need ---")
-        print(stock_data.head())
-    else:
+    if stock_data is None or stock_data.empty:
         print("--- Failed to fetch data ---")
+        sys.exit()
+    else:
+        print("stock data fetched successfully")
+    stock_return_data = rc.calculate_returns(stock_data)
+    print("stock data with returns:")
+    print(stock_return_data)
