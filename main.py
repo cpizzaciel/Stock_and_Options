8896f0b175539cbc084422082d81b12f7807436d
Xiangnan Sun <xiangnan.sun.apply@gmail.com>
2025-10-19 14:10:34 +0100

    add stock return calculation to main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     tickers = ['NVDA', 'KO']
     stock_data = spf.fetch_stock_price_history(tickers, '1y', '1d')
     if stock_data is None or stock_data.empty:
-        print("--- Failed to fetch data ---")
-        sys.exit()
+        error_message = f"Fatal Error: Failed to fetch data for ticker '{tickers}'. Program cannot continue."
+        sys.exit(error_message)
     else:
         print("stock data fetched successfully")
     stock_return_data = rc.calculate_returns(stock_data)
